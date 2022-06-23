2b43ce052f58eb7b9088012fcf95a28aacc0ec4e
dmitry <ds@gmail.com>
2022-06-23 17:03:44 +0300

    material Input component

--- src/material/Input/index.js
+++ src/material/Input/index.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
-export default function Input() {
+export default function Input({label = 'Value'}) {
   return (
     <Box
       component="form"
@@ -13,7 +13,7 @@ export default function Input() {
       autoComplete="off"
     >
       <div>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="outlined-basic" label="Outlined" label={label} variant="outlined" />
       </div>
     </Box>
   );
