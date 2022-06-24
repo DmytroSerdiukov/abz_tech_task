dca37849f24aefaf53c47d093162587421af7c18
dmitry <ds@gmail.com>
2022-06-24 16:03:03 +0300

    changed extension .js to .jsx

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+// import LOGO from '../images/Logo.svg'
+import { ReactComponent as Logo } from "../../images/Logo.svg";
+import Description from "./Description";
+
+import Button from "../../custom/Button";
+
+import styles from "./index.module.scss";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.container}>
+          <div className={styles.logo}>
+            <Logo />
+          </div>
+          <div className={styles.container_buttons}>
+            <Button text="Users" />
+            <Button text="Sign up" />
+          </div>
+        </div>
+      </header>
+      <body>
+        <Description />
+      </body>
+    </>
+  );
+};
+
+export default Header;
