dad34757b501e756ca5348dc78cd592248486148
dmitry <ds@gmail.com>
2022-06-24 12:46:09 +0300

    Users comp & styles refactor

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -8,31 +8,53 @@ import styles from "./index.module.scss";
 const users = [
   {
     title: "Salvador Stewart Flynn Thomas...",
-    content:
-      "Frontend Developer Frontend ... frontend_develop@gmail.com +38 (098) 278 44 24",
+    content: {
+      jobTitle: 'Frontend Developer Frontend ...',
+      email: 'frontend_develop@gmail.com',
+      phone: '+38 (098) 278 44 24'
+    }
   },
   {
     title: "Takamaru Ayako Jurrien",
-    content: "Lead Independent Director Takamuru@gmail.com +38 (098) 278 90 24",
+    content: {
+      jobTitle: 'Lead Independent Director',
+      email: 'Takamuru@gmail.com',
+      phone: '+38 (098) 278 90 24'
+    }
   },
   {
     title: "Ilya ",
-    content: "Co-Founder and CEO Ilya_founder@gmail.com +38 (098) 235 44 24",
+    content: {
+      jobTitle: 'Co-Founder and CEO',
+      email: 'Ilya_founder@gmail.com',
+      phone: '+38 (098) 235 44 24'
+    }
   },
   {
     title: "Alexandre",
-    content:
-      "Lead Independent Director Alexandr_develop@gmail.com +38 (098) 198 44 24",
+      content: {
+        jobTitle: 'Lead Independent Director',
+        email: 'Alexandr_develop@gmail.com',
+        phone: '+38 (098) 198 44 24'
+      }
   },
   {
     title: "Winny",
-    content:
-      "Former Senior Director Winny_develop@gmail.com +38 (098) 278 22 88",
+      content: {
+        jobTitle: 'Former Senior Director',
+        email: 'Winny_develop@gmail.com',
+        phone: '+38 (098) 278 22 88'
+      }
   },
   {
     title: "Simon",
-    content: "President of Commerce Simon@gmail.com +38 (098) 278 44 00",
+    content: {
+      jobTitle: 'President of Commerce',
+      email: 'Simon@gmail.com',
+      phone: '+38 (098) 278 44 00'
+    }
   },
+  
 ];
 
 const Users = () => {
@@ -42,13 +64,13 @@ const Users = () => {
         <h1 className={styles.header__title}>Working with GET request</h1>
       </header>
       <body className={styles.users__body}>
-        {users.map((user, i) => (
-          <UserCard title={user.title} content={user.content} />
+        {users.map((user, index) => (
+          <UserCard key={index} title={user.title} content={user.content} />
         ))}
       </body>
 
       <div className={styles.users__button}>
-        <Button text={"Show more"} />
+        <Button style={{width: '120px'}} text={"Show more"} />
       </div>
     </div>
   );
