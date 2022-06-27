e5fba2155bb5f57eb0acee7bd916b72e63a810ea
dmitry <ds@gmail.com>
2022-06-27 08:44:09 +0300

    Users comp side effect, refactor

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,64 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import UserCard from "../../custom/Card";
 import Button from "../../custom/Button";
 
 import styles from "./index.module.scss";
+import * as axios from "axios";
+import Loader from "../../custom/Loader";
 
-
-const users = [
-  {
-    title: "Salvador Stewart Flynn Thomas Salva...",
-    content: {
-      jobTitle: 'Leading specialist of the department of c...',
-      email: 'JeromeKlarkaJeromeKlarka1923362362...',
-      phone: '+38 (098) 278 76 24'
-    }
-  },
-  {
-    title: "Takamaru Ayako Jurrien",
-    content: {
-      jobTitle: 'Lead Independent Director',
-      email: 'Takamuru@gmail.com',
-      phone: '+38 (098) 278 90 24'
-    }
-  },
-  {
-    title: "Ilya ",
-    content: {
-      jobTitle: 'Co-Founder and CEO',
-      email: 'Ilya_founder@gmail.com',
-      phone: '+38 (098) 235 44 24'
-    }
-  },
-  {
-    title: "Alexandre",
-      content: {
-        jobTitle: 'Lead Independent Director',
-        email: 'Alexandr_develop@gmail.com',
-        phone: '+38 (098) 198 44 24'
-      }
-  },
-  {
-    title: "Winny",
-      content: {
-        jobTitle: 'Former Senior Director',
-        email: 'Winny_develop@gmail.com',
-        phone: '+38 (098) 278 22 88'
-      }
-  },
-  {
-    title: "Simon",
-    content: {
-      jobTitle: 'President of Commerce',
-      email: 'Simon@gmail.com',
-      phone: '+38 (098) 278 44 00'
-    }
-  },
-  
-];
+//for debug
+// const data = [
+//   {
+//     name: "Salvador Stewart Flynn Thomas Salva...",
+//       position: 'Leading specialist of the department of c...',
+//       email: 'JeromeKlarkaJeromeKlarka1923362362...',
+//       phone: '+38 (098) 278 76 24'
+//   },
+//   {
+//     name: "Takamaru Ayako Jurrien",
+//       position: 'Lead Independent Director',
+//       email: 'Takamuru@gmail.com',
+//       phone: '+38 (098) 278 90 24'
+//   },
+//   {
+//     name: "Ilya ",
+//       position: 'Co-Founder and CEO',
+//       email: 'Ilya_founder@gmail.com',
+//       phone: '+38 (098) 235 44 24'
+//   },
+//   {
+//     name: "Alexandre",
+//         position: 'Lead Independent Director',
+//         email: 'Alexandr_develop@gmail.com',
+//         phone: '+38 (098) 198 44 24'
+//   },
+//   {
+//     name: "Winny",
+//         position: 'Former Senior Director',
+//         email: 'Winny_develop@gmail.com',
+//         phone: '+38 (098) 278 22 88'
+//   },
+//   {
+//     name: "Simon",
+//     position: 'President of Commerce',
+//     email: 'Simon@gmail.com',
+//     phone: '+38 (098) 278 44 00'
+//   },
+// ];
 
 const Users = () => {
+
+  useEffect( () => {
+    getMoreUsers()
+  }, [])
+
+  const [users, setUsers] = useState([])
+  const [page, setPage] = useState(1)
+  const [isFetching, setFetching] = useState(false)
+
+  const getMoreUsers = async() => {
+    setFetching(true)
+    const response = await axios.get(`https://frontend-test-assignment-api.abz.agency/api/v1/users?page=${page}&count=6`)
+    const newUsers = response.data.users
+    setPage(page+1)
+    setUsers([...newUsers, ...users])
+    setFetching(false)
+  }
+
   return (
     <div className={styles.users__container}>
       <header className={styles.users__header}>
@@ -66,13 +73,19 @@ const Users = () => {
       </header>
       <body className={styles.users__body}>
         {users.map((user, index) => (
-          <UserCard key={index} title={user.title} content={user.content} />
+          <UserCard key={index} 
+            photo={user.photo}
+            name={user.name}
+            position={user.position}
+            email={user.email}
+            phone={user.phone}
+          />
         ))}
       </body>
-
+      {isFetching ? <Loader /> : 
       <div className={styles.users__button}>
-        <Button style={{width: '120px'}} text={"Show more"} />
-      </div>
+        <Button  fn={getMoreUsers} style={{width: '120px'}} text={"Show more"} />
+      </div>}
     </div>
   );
 };
