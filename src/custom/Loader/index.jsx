3fab0c5ab5db9c5225677795a0b8407911e0a0a7
dmitry <ds@gmail.com>
2022-06-27 22:16:59 +0300

    loader

---
+++ src/custom/Loader/index.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import styles from './index.module.scss'
+
+const Loader = () => {
+    return <div className={styles.card__loader}>
+  
+    </div>
+}
+
+export default Loader
