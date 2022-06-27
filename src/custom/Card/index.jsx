a844b58d2b4efec51dc9ec82ed520d9db453c5ee
dmitry <ds@gmail.com>
2022-06-27 08:21:42 +0300

    added loader, refactor Card component

--- src/custom/Card/index.jsx
+++ src/custom/Card/index.jsx
@@ -3,44 +3,53 @@ import styles from "./index.module.scss";
 
 import PhotoCover from "../../images/photo-cover.svg";
 
+
+const Loader = () => {
+  return <div className={styles.card__loader}>
+
+  </div>
+}
+
 //if jobTitle.length > 25 then
 //
 const ToolTip = ({ children, css, visibleSymbolsCount = 30 }) => {
   if (children.length > visibleSymbolsCount)
     return (
-      <span className={css || {}}>
+      <div className={css || {}}>
         {children.slice(0, visibleSymbolsCount) + "..."}
-        <span className={styles.card__tooltip}>{children}</span>
-      </span>
+        <div className={styles.card__tooltip}>{children}</div>
+      </div>
     );
   else return <span className={css || {}}>{children}</span>;
 };
 
-const UserCard = ({ title, content }) => {
-  const { jobTitle, email, phone } = content;
+const UserCard = (props) => {
+  console.log(props)
+  const {photo, name, position, email, phone } = props
 
   return (
     <div className={styles.card__container}>
-      <img className={styles.card__avatar} src={PhotoCover} alt="Avatar" />
+      {photo ? <img className={styles.card__avatar} src={photo} alt="Avatar" /> :      
+      <img className={styles.card__avatar} src={PhotoCover} alt="Avatar" /> }
       <h1 className={styles.card__title}>
-        <ToolTip>
-            {title}
+        <ToolTip visibleSymbolsCount={25}>
+            {name}
         </ToolTip>
       </h1>
       <div className={styles.card__content}>
-        <ToolTip css={styles.content__jobTitle}        >
-            {jobTitle}
+        <ToolTip
+          visibleSymbolsCount={30}
+        >
+            {position}
         </ToolTip>
-        <ToolTip css={styles.content__jobTitle}>
+        <ToolTip css={styles.content__email}
+          visibleSymbolsCount={25}
+        >
             {email}
         </ToolTip>
         <ToolTip css={styles.content__phone}>
             {phone}
         </ToolTip>
-        {/* <span className={styles.content__jobTitle}>
-        </span> */}
-        {/* <span className={styles.content__email}>{email}</span> */}
-        {/* <span className={styles.content__phone}>{phone}</span> */}
       </div>
     </div>
   );
