e4f4cfb71c8b7f092471f0a0e6abf62e1f501e19
dmitry <ds@gmail.com>
2022-06-27 22:21:40 +0300

    Get request for new portion of users, Form

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,19 +1,38 @@
 import { Button } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Buttons from "../../custom/Button";
 import Input from "../../material/Input";
 import RadioButton from "../../material/RadioButton";
 import FileUploader from "../../custom/FileUploader";
-
+import Form from './Form'
 import styles from "./index.module.scss";
+import * as axios from 'axios'
+
 
 const Register = () => {
+
+  const [positions, setPositions] = useState([])
+
+  useEffect( () => {
+    getPositions()
+  }, [])
+
+  const getPositions = async() => {
+    const response = await axios.get('https://frontend-test-assignment-api.abz.agency/api/v1/positions')
+    setPositions(response.data.positions)
+    console.log(response.data)
+  }
+
+
   return (
     <div className={styles.register__container}>
       <header className={styles.container__header}>
         <h1 className={styles.header__h1}>Working with POST request</h1>
       </header>
-      <div className={styles.register__form}>
+      <Form positions = {positions} />
+      <div className={styles.register__radio}>
+        </div>
+      {/* <div className={styles.register__form}>
         <Input label="Your Name" />
         <Input label="Email" />
         <Input label="Phone" />
@@ -26,7 +45,7 @@ const Register = () => {
         <div className={styles.register__button}>
           <Buttons text={"Sign up"} disabled />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
