c16a5c2f6b2b854abe17fda67377ad53932558e7
dmitry <ds@gmail.com>
2022-07-02 15:53:07 +0300

    custom hook provide user data

--- src/components/Users/container.jsx
+++ src/components/Users/container.jsx
@@ -2,29 +2,13 @@ import React, { useEffect, useState } from 'react'
 import Users from '.'
 
 import * as axios from 'axios'
+import useUserData from '../../hooks/fetchUsers'
 
 const UsersContainer = () => {
 
-    useEffect( () => {
-        fetchUsers()
-      }, [])
-    
-    const [users, setUsers] = useState([])
-    const [page, setPage] = useState(1)
+  const userData = useUserData()
 
-    const fetchUsers = async() => {
-        try {
-            const response = await axios.get(`https://frontend-test-assignment-api.abz.agency/api/v1/users?page=${page}&count=6`)
-            const newUsers = response.data.users
-            setPage(page+1)
-            setUsers([...newUsers, ...users])
-          } catch (e) {
-            alert(e)
-        }
-    }
-
-
-    return <Users fetchUsers={fetchUsers} users={users}/>
+  return <Users {...userData}/>
 }
 
 export default UsersContainer
---
+++ src/hooks/fetchUsers.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import * as axios from 'axios'
+
+const useUserData = () => {
+
+    const [page, setPage] = useState(1)
+    const [users, setUsers] = useState([])
+    const [success, setSuccess] = useState()
+    const url = `https://frontend-test-assignment-api.abz.agency/api/v1/users?page=${page}&count=6`
+
+    const fetchUsers = async() => {
+        try {
+            const response = await axios.get(url)
+            const newUsers = response.data.users
+            setPage(page+1)
+            setUsers([...newUsers, ...users])
+            setSuccess(response.data.success)
+          } catch (e) {
+            setSuccess(false)
+        }
+    }
+
+    useEffect( () => {
+        fetchUsers()
+    }, [])
+
+
+    return {users, success, fetchUsers}
+
+}
+
+export default useUserData
