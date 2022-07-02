166d038c3357f25d64c1db3739101de871c95803
dmitry <ds@gmail.com>
2022-07-02 14:06:33 +0300

    radio buttons

--- src/material/RadioButton/index.jsx
+++ src/material/RadioButton/index.jsx
@@ -1,63 +1,37 @@
-import {
-  FormControl,
-  FormControlLabel,
-  FormLabel,
-  RadioGroup,
-} from "@mui/material";
 import React, { useContext } from "react";
+
+import Radio from "@mui/material/Radio";
+import FormLabel from "@mui/material/FormLabel";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
 import { RegisterContext } from "../../context/RegisterContext";
+
 import styles from './index.module.scss'
 
-
-const Radio = ({ name, value }) => {
+export default function RadioButtonsGroup() {
+  const {positions} = useContext(RegisterContext);
   return (
-    <div>
-      <input type="radio" id="contactChoice1" name={name} value={value} />
-      <label for="contactChoice1">{name}</label>
-    </div>
+    <FormControl classes={styles.radio__form} >
+      <FormLabel sx={{marginBottom: '0px'}} id="demo-radio-buttons-group-label" >
+        Select your position
+      </FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-radio-buttons-group-label"
+        defaultValue="female"
+        name="radio-buttons-group"
+      >
+        {positions.map((el, i) => (
+          <FormControlLabel
+            key={el.id}
+            value={el.name}
+            control={<Radio size="small" sx={{fontSize: '120px'}}/>}
+            label={el.name}
+            
+          />
+        ))}
+      </RadioGroup>
+    </FormControl>
   );
-};
-
-const RadioButton = () => {
-
-  const positions = useContext(RegisterContext)
-  return (
-    <form className={styles.radio__form}>
-      <p className={styles.form__header}>Select your position</p>
-      <div className={styles.radio__list}>
-        {positions.map( (el, i) => <Radio key={el.id} name={el.name} value={el.name} />)}
-        {/* <Radio name="Front-Developer" value="10" />
-        <Radio name="Backend developer" value="10" />
-        <Radio name="Designer" value="10" />
-        <Radio name="QA" value="10" /> */}
-
-        {/* <input type="radio" id="contactChoice1" name="contact" value="email" />
-        <label for="contactChoice1">F</label>
-        <input type="radio" id="contactChoice1" name="contact" value="email" />
-        <label for="contactChoice1">Email</label>
-        <input type="radio" id="contactChoice1" name="contact" value="email" />
-        <label for="contactChoice1">Email</label>
-        <input type="radio" id="contactChoice1" name="contact" value="email" />
-        <label for="contactChoice1">Email</label>
-        <input type="radio" id="contactChoice1" name="contact" value="email" />
-        <label for="contactChoice1">Email</label> */}
-      </div>
-    </form>
-  );
-
-  //   <FormControl>
-  //   <FormLabel id="demo-radio-buttons-group-label">Select your position</FormLabel>
-  //   <RadioGroup
-  //     aria-labelledby="demo-radio-buttons-group-label"
-  //     defaultValue="female"
-  //     name="radio-buttons-group"
-  //   >
-  //     <FormControlLabel value="Frontend developer" control={<Radio />} label="Frontend developer" />
-  //     <FormControlLabel value="Backend developer" control={<Radio />} label="Backend developer" />
-  //     <FormControlLabel value="Designer" control={<Radio />} label="Designer" />
-  //     <FormControlLabel value="QA" control={<Radio />} label="QA" />
-  //   </RadioGroup>
-  // </FormControl>
-};
-
-export default RadioButton;
+}
