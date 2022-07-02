5a9b2ddc38c5d1fb979c0e55ed9f876942b0350d
dmitry <ds@gmail.com>
2022-07-02 14:12:41 +0300

    added tip from react-tooltip

--- src/custom/Tooltip/index.jsx
+++ src/custom/Tooltip/index.jsx
@@ -1,55 +1,50 @@
 import React, { useState } from 'react'
+import ReactTooltip from 'react-tooltip'
 
 import styles from './index.module.scss'
 
-const Tooltip = ({children, content, visibleSymbolsCount = 30}) => {
+const ToolTip = ({children, direction='bottom'}) => {
+  
+  
+  const [visible, setVisible] = useState(false)
 
-    const [active, setActive] = useState(false)
-
-    const showTip = () => {
-        setActive(true)
-    }
-
-    const hideTip = () => {
-        setActive(false)
-    }
-
-    if (children.length > visibleSymbolsCount)
-        return <div 
-        className={styles.tooltip__wrapper}
-        onMouseEnter={showTip}
-        onMouseLeave={hideTip}
-    >
-        <span>
-            {children.slice(0, visibleSymbolsCount) + "..."}
-        </span>
-        {active && <div className={styles.tooltip__tip}>
-            {content}
-        </div>}
-    </div>
-
-    else return <div 
-        className={styles.tooltip__wrapper}
-    >
-        <span>
+  const showTip = () => setTimeout(() => setVisible(true), 300)
+  
+  const hideTip = () => setVisible(false)
+  
+  if (children.length > 25)
+    return <div>
+        <div data-tip data-for={`${children}`}>{children.slice(0,25)+'...'}</div>
+        <ReactTooltip id={`${children}`} place='bottom' effect='solid'>
             {children}
-        </span>
+        </ReactTooltip>
     </div>
+  else return <div>
+    {children}
+  </div>
 }
 
-export default Tooltip
+    // if (children.length > visibleSymbolsCount)
+    //     return <div 
+    //     className={styles.tooltip__wrapper}
+    //     onMouseEnter={showTip}
+    //     onMouseLeave={hideTip}
+    // >
+    //     {children.slice(0, visibleSymbolsCount) + "..."}
+    //     {active && <div className={styles.tooltip__tip}>
+    //         {children}
+    //     </div>}
+    // </div>
 
+    // else return <div 
+    //     className={styles.tooltip__wrapper}
+    // >
+    //     <div>
+    //         {children}
+    //     </div>
+    // </div>
+// }
 
-//if jobTitle.length > 25 then
-//
-const ToolTip = ({ children, css, visibleSymbolsCount = 30 }) => {
-    if (children.length > visibleSymbolsCount)
-      return (
-        <div className={css || {}}>
-          {children.slice(0, visibleSymbolsCount) + "..."}
-          <div className={styles.card__tooltip}>{children}</div>
-        </div>
-      );
-    else return <span className={css || {}}>{children}</span>;
-  };
+export default ToolTip
+
   
