00979cf0de8db7cc8ff383ea1a71c1e80457fab7
dmitry <ds@gmail.com>
2022-07-02 14:17:19 +0300

    card refactor

--- src/custom/Card/index.jsx
+++ src/custom/Card/index.jsx
@@ -2,53 +2,36 @@ import React from "react";
 import styles from "./index.module.scss";
 
 import PhotoCover from "../../images/photo-cover.svg";
-
+import Tooltip from "../Tooltip";
 
 const Loader = () => {
-  return <div className={styles.card__loader}>
-
-  </div>
-}
-
-//if jobTitle.length > 25 then
-//
-const ToolTip = ({ children, css, visibleSymbolsCount = 30 }) => {
-  if (children.length > visibleSymbolsCount)
-    return (
-      <div className={css || {}}>
-        {children.slice(0, visibleSymbolsCount) + "..."}
-        <div className={styles.card__tooltip}>{children}</div>
-      </div>
-    );
-  else return <span className={css || {}}>{children}</span>;
+  return <div className={styles.card__loader}></div>;
 };
 
+
 const UserCard = (props) => {
-  const {photo, name, position, email, phone } = props
+  const { photo, name, position, email, phone } = props;
+
+  const addDefaultSrc = (event) => {
+    event.target.src = PhotoCover
+  }
 
   return (
     <div className={styles.card__container}>
-      {photo ? <img className={styles.card__avatar} src={photo} alt="Avatar" /> :      
-      <img className={styles.card__avatar} src={PhotoCover} alt="Avatar" /> }
-      <h1 className={styles.card__title}>
-        <ToolTip visibleSymbolsCount={25}>
-            {name}
-        </ToolTip>
-      </h1>
-      <div className={styles.card__content}>
-        <ToolTip
-          visibleSymbolsCount={30}
-        >
-            {position}
-        </ToolTip>
-        <ToolTip css={styles.content__email}
-          visibleSymbolsCount={25}
-        >
-            {email}
-        </ToolTip>
-        <ToolTip css={styles.content__phone}>
-            {phone}
-        </ToolTip>
+      <img className={styles.card__avatar} onError={addDefaultSrc} src={photo} alt="" />
+      <Tooltip>
+        {name}
+      </Tooltip>
+      <div style={{marginTop: '20px'}}>
+        <Tooltip>
+          {position}
+        </Tooltip>
+        <Tooltip>
+          {email}
+        </Tooltip>
+        <Tooltip>
+          {phone}
+        </Tooltip>
       </div>
     </div>
   );
