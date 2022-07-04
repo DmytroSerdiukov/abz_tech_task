8b32dd85403607b97b444130f4eac68554a4f997
dmitry <ds@gmail.com>
2022-07-04 13:17:29 +0300

    uploader refactor

--- src/custom/Uploader/index.jsx
+++ src/custom/Uploader/index.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import PhotoCamera from '@mui/icons-material/PhotoCamera';
@@ -10,24 +10,20 @@ import styles from './index.module.scss'
 
 
 
-export default function UploadButtons() {
+export default function Uploader() {
+
+  useEffect( () =>console.log('file value',file.current) )
+  const file = useRef(null)
+  if(file)
+    console.log('file value',file.value)
   return (<>
-    {/* // <Button styles={{padding: '14px 15px', border: '1px solid black'}}> 
-    //     Upload
-    //     <TextField type='file'></TextField>
-    // </Button>
-    
-    // <div style={{display: 'flex', flexDirection:'row'}}>
-    //     <Button variant='outlined' style={{borderRadius: '0px'}}>Upload</Button>
-    //     <TextField  style={{display: 'none'}} type='file' />
-    // </div> */}
-    <label className={styles.unploader__container}>
-      <div style={{display: 'flex'}}>
+    <label>
+      <div style={{display: 'flex', height:'54px', marginTop: '14px', marginBottom: '50px', overflow: 'hidden'}}>
         <div className={styles.input__label}>Upload</div>
         <div className={styles.input__field}>Upload your file</div>
       </div>
-      <div>
-        <Input type='file' className={styles.uploader__input}/>
+      <div style={{display: 'none'}}>
+        <Input ref={file}  type='file' className={styles.uploader__input}/>
       </div>
     </label>
     
