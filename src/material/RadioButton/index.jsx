afa52d98f7109e25c564594649bc6930ff313924
dmitry <ds@gmail.com>
2022-07-04 21:59:32 +0300

    radiobuttons refactor

--- src/material/RadioButton/index.jsx
+++ src/material/RadioButton/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import Radio from "@mui/material/Radio";
 import FormLabel from "@mui/material/FormLabel";
@@ -6,15 +6,32 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControl from "@mui/material/FormControl";
 import FormControlLabel from "@mui/material/FormControlLabel";
 
-import { RegisterContext } from "../../context/RegisterContext";
-
 import styles from './index.module.scss'
 
+import useRadio from "../../hooks/selectRadioButton";
+import { RegisterContext } from "../../context/RegisterContext";
+
 export default function RadioButtonsGroup() {
+
+  const {selectedValue, onValueChange} = useRadio()
   const {positions} = useContext(RegisterContext);
+  
+  const getCurrentValue = (event) => {
+    onValueChange(event.target.value)
+  }
+
   return (
     <FormControl classes={styles.radio__form} >
-      <FormLabel sx={{marginBottom: '0px'}} id="demo-radio-buttons-group-label" >
+      <FormLabel
+        sx={{marginBottom: '6px',
+          fontSize: '1rem',
+          lineHeight:'1.625rem',
+          color: 'black',
+          fontFamily: 'Nunito',
+          fontWeight: 400
+        }}
+        id="demo-radio-buttons-group-label"
+      >
         Select your position
       </FormLabel>
       <RadioGroup
@@ -26,9 +43,21 @@ export default function RadioButtonsGroup() {
           <FormControlLabel
             key={el.id}
             value={el.name}
-            control={<Radio size="small" sx={{fontSize: '120px'}}/>}
             label={el.name}
-            
+            control={
+              <Radio size="medium"
+                checked={selectedValue === el.name}
+                onChange={onValueChange}
+                value={el.name}
+                inputProps={{
+                  fontSize: '1rem',
+                  lineHeight:'1.625rem',
+                  color: 'black',
+                  fontFamily: 'Nunito',
+                  fontWeight: 400
+                }}
+                style={{ marginTop:'-6px', paddingBottom: 7, }}
+              />}
           />
         ))}
       </RadioGroup>
