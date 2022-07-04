53e80010514bc758944e7c5d6fe98adc64dd7ba7
dmitry <ds@gmail.com>
2022-07-04 13:17:03 +0300

    input refactor

--- src/material/Input/index.jsx
+++ src/material/Input/index.jsx
@@ -2,19 +2,44 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
+
 export default function Input({label = 'Value', ...props}) {
   return (
     <Box
       component="form"
       sx={{
-        "& .MuiTextField-root": { fontSize: '1rem', marginBottom: '50px', width: "380px" },
+        "& .MuiTextField-root": { fontSize: '1rem',
+        marginTop: '-16px', 
+        marginBottom: '49px', 
+        border: 'none'
+        },
       }}
       noValidate
       autoComplete="off"
     >
       <div>
-        <TextField id="outlined-basic" {...props}  label={label} variant="outlined" />
+        <TextField id="outlined-basic" 
+        
+        InputLabelProps={{
+          style: { border: 'none',
+          fontFamily: 'Nunito',
+          padding: '12px 16px',
+          zIndex: 1
+        }
+        }}
+        InputProps={{disableUnderline: true, style: style}}
+         {...props}  label={label} variant='standard'/>
       </div>
     </Box>
   );
+}
+
+const style = {
+  border: '1px solid #D0CFCF',
+  outline: 'false',
+  borderRadius: '4px',
+  width:380,
+  height: 55,
+  fontSize: '1rem',
+  lineHeight: '1.3rem'
 }
