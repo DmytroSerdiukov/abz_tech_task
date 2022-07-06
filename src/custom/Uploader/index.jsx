7277bd1a827ddfae5e41306a591b6b443caceff8
dmitry <ds@gmail.com>
2022-07-06 13:03:33 +0300

    uploader refactor

--- src/custom/Uploader/index.jsx
+++ src/custom/Uploader/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import PhotoCamera from '@mui/icons-material/PhotoCamera';
@@ -6,24 +6,27 @@ import Stack from '@mui/material/Stack';
 import { TextField, RaisedButtond } from '@mui/material';
 import Input from '../../material/Input'
 
-import styles from './index.module.scss'
+import uploader from './uploader.module.scss'
 
+export default function Uploader({name, value}) {
 
+  const input = useRef(null)
+  const [file, setFile] = useState()
 
-export default function Uploader() {
+  const handleChange = (event) => {
+    setFile(event.target.files[0])
+    value = event.target.files[0]
+    console.log('file', value)
+  }
 
-  useEffect( () =>console.log('file value',file.current) )
-  const file = useRef(null)
-  if(file)
-    console.log('file value',file.value)
   return (<>
     <label>
-      <div style={{display: 'flex', height:'54px', marginTop: '14px', marginBottom: '50px', overflow: 'hidden'}}>
-        <div className={styles.input__label}>Upload</div>
-        <div className={styles.input__field}>Upload your file</div>
+      <div style={{display: 'flex', height:'54px', marginTop: '47px', marginBottom: '50px', overflow: 'hidden'}}>
+        <div className={uploader.input__label}>Upload</div>
+        <div className={uploader.input__field}>{ file?.name || 'Upload your file'}</div>
       </div>
       <div style={{display: 'none'}}>
-        <Input ref={file}  type='file' className={styles.uploader__input}/>
+        <Input name={name} ref={input} onChange={handleChange}  type='file' className={uploader.uploader__input}/>
       </div>
     </label>
     
