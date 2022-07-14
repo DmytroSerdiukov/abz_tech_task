395aea14c2faf11ec566ed494ae5eb5e067749f9
dmitry <ds@gmail.com>
2022-07-14 12:04:35 +0300

    test

--- src/custom/Card/index.jsx
+++ src/custom/Card/index.jsx
@@ -8,30 +8,28 @@ const Loader = () => {
   return <div className={styles.card__loader}></div>;
 };
 
-
 const UserCard = (props) => {
   const { photo, name, position, email, phone } = props;
 
   const addDefaultSrc = (event) => {
-    event.target.src = PhotoCover
-  }
+    event.target.src = PhotoCover;
+  };
 
   return (
     <div className={styles.card__container}>
-      <img className={styles.card__avatar} onError={addDefaultSrc} src={photo} alt="" />
-      <Tooltip>
-        {name}
-      </Tooltip>
-      <div style={{paddingTop: '20px'}}>
-        <Tooltip>
-          {position}
-        </Tooltip>
-        <Tooltip>
-          {email}
-        </Tooltip>
-        <Tooltip>
-          {phone}
-        </Tooltip>
+      <img
+        className={styles.card__avatar}
+        onError={addDefaultSrc}
+        src={photo}
+        alt=""
+      />
+      <div style={{ paddingTop: "20px" }}>
+        <Tooltip>{name}</Tooltip>
+      </div>
+      <div style={{ paddingTop: "20px" }}>
+        <Tooltip>{position}</Tooltip>
+        <Tooltip>{email}</Tooltip>
+        <Tooltip>{phone}</Tooltip>
       </div>
     </div>
   );
