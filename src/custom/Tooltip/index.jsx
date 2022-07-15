e42066769021e3539b7990526d46eb6a64ed5afa
dmitry <ds@gmail.com>
2022-07-15 10:31:13 +0300

    tooltip refactor

--- src/custom/Tooltip/index.jsx
+++ src/custom/Tooltip/index.jsx
@@ -1,17 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactTooltip from 'react-tooltip'
 
-import styles from './index.module.scss'
 
 const ToolTip = ({children, direction='bottom'}) => {
-  
-  
-  const [visible, setVisible] = useState(false)
-
-  const showTip = () => setTimeout(() => setVisible(true), 300)
-  
-  const hideTip = () => setVisible(false)
-  
   if (children.length > 25)
     return <div>
         <div data-tip data-for={`${children}`}>{children.slice(0,25)+'...'}</div>
@@ -23,27 +14,6 @@ const ToolTip = ({children, direction='bottom'}) => {
     {children}
   </div>
 }
-
-    // if (children.length > visibleSymbolsCount)
-    //     return <div 
-    //     className={styles.tooltip__wrapper}
-    //     onMouseEnter={showTip}
-    //     onMouseLeave={hideTip}
-    // >
-    //     {children.slice(0, visibleSymbolsCount) + "..."}
-    //     {active && <div className={styles.tooltip__tip}>
-    //         {children}
-    //     </div>}
-    // </div>
-
-    // else return <div 
-    //     className={styles.tooltip__wrapper}
-    // >
-    //     <div>
-    //         {children}
-    //     </div>
-    // </div>
-// }
 
 export default ToolTip
 
